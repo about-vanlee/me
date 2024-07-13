f61893e089f8f18714dfb9673df98eef182fcef6
yayxs <leaderywl@163.com>
2024-07-13 10:48:33 +0800

    feat: dev about text

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2>Hello</h2>
-  <p>World</p>
+  <main>
+    <section class="title">关于</section>
+    <section class="content"></section>
+  </main>
 </template>
 
-<style></style>
+<style>
+body {
+  background-color: #fff;
+  border: none;
+  color: #000;
+  font-size: 18px;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  min-height: 100%;
+}
+
+.title {
+  margin-top: 0;
+  font-weight: 700;
+  margin-bottom: 30px;
+  text-align: center;
+  line-height: 1.4;
+}
+</style>
